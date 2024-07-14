3c2635b5740b2f9208e2ee7c34e9a8fb1fd01e5c
Asbyx <elrbergeron@gmail.com>
2024-07-14 13:09:03 +0200

    Implemented UI for the config

--- src/main/java/io/github/asbyx/autonomousstreamset/IndexController.java
+++ src/main/java/io/github/asbyx/autonomousstreamset/IndexController.java
@@ -1,14 +1,40 @@
 package io.github.asbyx.autonomousstreamset;
 
+import io.github.asbyx.autonomousstreamset.config.ConfigManager;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class IndexController {
 	@FXML
-	private Label welcomeText;
+	public TextField streamName;
 
 	@FXML
-	protected void onHelloButtonClick() {
-		welcomeText.setText("Welcome to JavaFX Application!");
+	public TextField APIToken;
+
+	@FXML
+	public TextField tournamentSlug;
+
+	@FXML
+	public TextField eventSlug;
+
+	@FXML
+	public void start(ActionEvent actionEvent) {
+		// use the ConfigManager class to write the config file
+		ConfigManager.writeConfig(streamName.getText(), APIToken.getText(), tournamentSlug.getText(), eventSlug.getText());
+
+		//todo: start the app
+	}
+
+	@FXML
+	public void initialize() {
+		// use the ConfigManager class to read the config file
+		String[] config = ConfigManager.readConfig();
+		streamName.setText(config[0]);
+		APIToken.setText(config[1]);
+		tournamentSlug.setText(config[2]);
+		eventSlug.setText(config[3]);
+
 	}
 }
